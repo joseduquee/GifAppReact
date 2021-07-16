80adb3eae5e82ba0184fb60fa2b369dd4c143d2d
Alexander Duque <joseduquee@gmail.com>
2021-07-16 15:46:04 +0200

    first commit gifs app react

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+//referencia
+
+export const GifExpertApp = () => {
+
+    const [categorias, setCategorias] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategorias={ setCategorias } />
+            <hr />
+
+            <ol>
+                {
+                    categorias.map( categoria => (
+                        <GifGrid 
+                            key={ categoria }
+                            categoria={ categoria } 
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
+
---
+++ src/components/AddCategory.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export const AddCategory = ( { setCategorias } ) => {
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue( e.target.value );
+    }
+
+    const handleSubmit = (e) => {
+        //esto evita que se refresque a cada rato
+        e.preventDefault();
+
+        if( inputValue.trim().length > 2 ) {
+
+            setCategorias( c => [inputValue, ...c ]);
+            setInputValue('');
+        };
+
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input
+                type="text"
+                value={ inputValue }
+                onChange={ handleInputChange }
+
+            />
+        </form>
+    )
+};
+
+AddCategory.propTypes = {
+    setCategorias: PropTypes.func.isRequired
+};
